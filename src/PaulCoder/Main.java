9370f1eb1d6162a37076e02e20e555dcec8f7969
Pawciio <choldapawel@gmail.com>
2020-11-06 15:06:52 +0100

    create method

--- src/PaulCoder/Main.java
+++ src/PaulCoder/Main.java
@@ -4,26 +4,40 @@ public class Main {
 
     public static void main(String[] args) {
 
+        printLetter();
+        printBoard();
+
+    }
+
+    static void printLetter(){
         System.out.print("  ");
         for (int i = 0; i < 10; i++) {
             System.out.print((char)('A' +i) );
 
         }
         System.out.print('\n');
+    }
+
+    static void printBoard(){
         for (int i = 1; i <= 10; i++) {
             if (i < 10){
                 System.out.print('0');
             }
             System.out.print(i);
             for (int j = 0; j < 10; j++) {
-                if (Math.random() < 0.2){
-                    System.out.print('O');
-                }else{
-                    System.out.print(' ');
-                }
+                char shipValue = getRandomShip();
+                System.out.print(shipValue);
 
             }
             System.out.print('\n');
+        }
+    }
+
+    private static char getRandomShip() {
+        if (Math.random() < 0.2){
+            return 'O';
+        }else{
+            return ' ';
         }
     }
 }
